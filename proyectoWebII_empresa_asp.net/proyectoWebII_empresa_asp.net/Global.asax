7cf07b412e946b97594dcb3a367a6d8e76c7dc1e
Kevin Rodriguez <kevin.1296@hotmail.com>
2017-07-12 09:42:31 -0600

    Creacion de cuentas individuales

---
+++ proyectoWebII_empresa_asp.net/proyectoWebII_empresa_asp.net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="proyectoWebII_empresa_asp.net.MvcApplication" Language="C#" %>
---
+++ proyectoWebII_empresa_asp.net/proyectoWebII_empresa_asp.net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(proyectoWebII_empresa_asp.net.Startup))]
+namespace proyectoWebII_empresa_asp.net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
